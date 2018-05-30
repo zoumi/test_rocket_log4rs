7c64855787de95bf21b370bdf8c6eef65dec72bd
zoumi <aaasuoliweng@126.com>
2018-05-30 21:23:06 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+#![feature(plugin)]
+#![feature(custom_derive)]
+#![plugin(rocket_codegen)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![feature(use_extern_macros)]
+#![feature(proc_macro_path_invoc)] 
+#![feature(custom_attribute)] 
+#![feature(decl_macro)] 
+#![feature(attr_literals)]
+#![feature(unicode)]  
+#![feature(try_from)] 
+extern crate rocket;
+use rocket::http::Status;
+use rocket::http::RawStr;
+use rocket::request::{self, Form, FromForm, FromRequest};
+use rocket::response::{content, NamedFile};
+use rocket::{Outcome, Request, State};
+use rocket::config::{self, Config, Environment, RocketConfig};
+extern crate rocket_contrib;
+use rocket_contrib::Template;
+
+
+//extern crate env_logger;
+#[macro_use(log)]
+extern crate log;
+extern crate log4rs;
+
+pub test_mod;
+
+#[get("/home")]
+fn home(s_op:Option<Session>) -> String {
+    ::log::debug!("debug");
+    ::log::info!("info");
+    ::log::warn!("warn");
+    ::log::error!("error");
+    test_mod::t();
+    "home".to_owned()
+}
+
+//#[catch(404)]    //rocket 4.0 dev
+#[error(404)]
+fn not_found(req: &rocket::Request) -> content::Html<&'static str> {
+    content::Html("Page Not Found.")
+}
+
+
+fn main() {
+    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
+
+    rocket::ignite()
+        .mount("/", routes![home])
+        .catch(errors![not_found])
+        .launch();
+}
---
+++ src/test_mod.rs
@@ -0,0 +1,6 @@
+pub fn t(){
+    ::log::debug!("debug");
+    ::log::info!("info");
+    ::log::warn!("warn");
+    ::log::error!("error");
+}
